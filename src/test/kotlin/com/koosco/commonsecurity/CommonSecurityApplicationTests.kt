8f64aac60ab4960302194f24522b24122ff53e72
Koo <koosco136@gmail.com>
2025-11-24 00:32:36 +0900

    jwt parsing for common usage

---
+++ src/test/kotlin/com/koosco/commonsecurity/CommonSecurityApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.koosco.commonsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
